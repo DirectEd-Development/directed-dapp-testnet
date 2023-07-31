33cdb78718898fde6c9be5e09f75aab02a0084e5
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-07-31 11:52:22 +0100

    fix: add back to home buttons

--- pages/mint-tokens/index.tsx
+++ pages/mint-tokens/index.tsx
@@ -1,15 +1,19 @@
-import { Layout } from '../../components';
+import { Button, Layout } from '../../components';
 import type { NextPage } from "next";
+import Link from 'next/link'; // Import the Link component
 
 const Redeem: NextPage = () => {
-  return (
-    <Layout>
-      <div className='crowdfunding-page'>
-        <h1>Crowdfunding-Milestones Page</h1>
-        <p>This page is currently under construction. It will be available soon.</p>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className='crowdfunding-page'>
+                <h1>Crowdfunding-Milestones Page</h1>
+                <p>This page is currently under construction. It will be available soon.</p>
+                <Link href="/">
+                    <Button>Back to Home</Button>
+                </Link>
+            </div>
+        </Layout>
+    );
 };
 
 export default Redeem;
