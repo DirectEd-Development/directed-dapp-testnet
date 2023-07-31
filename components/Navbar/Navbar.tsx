76318055ba6422def3d93ca12542b735df5367af
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-07-31 11:26:23 +0100

    fix: buttons for test landing page

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,11 +10,11 @@ type LinksType = {
 }
 
 const NavLinks: LinksType[] = [
-	{ title: 'Why DirectEd?', link: '/' },
+	{ title: 'Home', link: '/' },
 	// { title: 'Scholarship Crowdfunding', link: '/scholarship-pool' },
 	// { title: "Donor's Portal", link: '/donors-portal/nft-gallery' },
-	{title: "Mint Tokens", link: "/mint-tokens"},
-	{ title: 'About Us', link: 'https://www.notion.so/directed/About-Us-1be24f1a03ca4c1298dfb9ed20007d2a'}
+	// {title: "Mint Tokens", link: "/mint-tokens"},
+	// { title: 'About Us', link: 'https://www.notion.so/directed/About-Us-1be24f1a03ca4c1298dfb9ed20007d2a'}
 ]
 
 const Navbar = () => {
--- next.config.js
+++ next.config.js
@@ -1,41 +1,46 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	webpack(config, { isServer, dev }) {
-		config.experiments = {
-			asyncWebAssembly: true,
-			layers: true,
-		}
-
-		if (!dev && isServer) {
-			config.output.webassemblyModuleFilename = 'chunks/[id].wasm'
-			config.plugins.push(new WasmChunksFixPlugin())
-		}
-
-		config.resolve.fallback = {
-			fs: false,
-			module: false,
-		}
-
-		return config
+	images: {
+	  domains: ['kfdniefadaanbjodldohaedphafoffoh'],
 	},
-	reactStrictMode: true
-}
+	webpack(config, { isServer, dev }) {
+	  config.experiments = {
+		asyncWebAssembly: true,
+		layers: true,
+	  };
+  
+	  if (!dev && isServer) {
+		config.output.webassemblyModuleFilename = 'chunks/[id].wasm';
+		config.plugins.push(new WasmChunksFixPlugin());
+	  }
+  
+	  config.resolve.fallback = {
+		fs: false,
+		module: false,
+	  };
+  
+	  return config;
+	},
+	reactStrictMode: true,
+  };
+  
 
-class WasmChunksFixPlugin {
+  class WasmChunksFixPlugin {
 	apply(compiler) {
-		compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
-			compilation.hooks.processAssets.tap(
-				{ name: 'WasmChunksFixPlugin' },
-				(assets) =>
-					Object.entries(assets).forEach(([pathname, source]) => {
-						if (!pathname.match(/\.wasm$/)) return
-						compilation.deleteAsset(pathname)
-
-						const name = pathname.split('/')[1]
-						const info = compilation.assetsInfo.get(pathname)
-						compilation.emitAsset(name, source, info)
-					})
-			)
-		})
+	  compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
+		compilation.hooks.processAssets.tap(
+		  { name: 'WasmChunksFixPlugin' },
+		  (assets) =>
+			Object.entries(assets).forEach(([pathname, source]) => {
+			  if (!pathname.match(/\.wasm$/)) return;
+			  compilation.deleteAsset(pathname);
+  
+			  const name = pathname.split('/')[1];
+			  const info = compilation.assetsInfo.get(pathname);
+			  compilation.emitAsset(name, source, info);
+			})
+		);
+	  });
 	}
-}
+  }
+  
