55e91a16e8005cd4cd652f1b8ada4ff1ac9cfdd0
jonniek01 <jontejonhn@gmail.com>
2023-08-03 13:38:01 +0300

    getting utxos

--- pages/redeem/index.tsx
+++ pages/redeem/index.tsx
@@ -1,8 +1,56 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useWallet, useWalletList } from '@meshsdk/react'
+
 import { Button, Layout } from '../../components';
 import type { NextPage } from "next";
 
+/*
+Connect wallet
+Get utxos from connected wallet
+*/
+
+interface Wallet {
+	name: string
+	icon: string
+}
+
 const Redeem: NextPage = () => {
+	const { connected, wallet } = useWallet()
+    //get utxos
+    const [utxos, setUtxos] = useState<any>([])
+    const [wallets, setWallets] = useState<Wallet[]>([])
+
+    const walletList = useWalletList()
+
+    useEffect(() => {
+        if (walletList) {
+            setWallets(walletList)
+        }
+        console.log("wallets: ", wallets)
+    }
+    , [walletList])
+
+
+
+    const getUtxos = async () => {
+        if (connected && wallet) {
+            const utxos = await wallet.getUtxos()
+            setUtxos(utxos)
+        }
+    }
+
+    useEffect(() => {
+        getUtxos()
+        console.log("utxos: ", utxos)
+    }
+    , [connected, wallet])
+
+
+
+
+  
+
     return (
         <Layout>
             <div className='redeem-page'>
