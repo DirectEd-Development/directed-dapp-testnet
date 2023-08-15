1bddf4b4f9ae7226d79db8f7c986b838ae5ef686
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-08-15 16:29:46 +0100

    fix: remove seed

--- pages/redeem/seed.js
+++
@@ -1,1 +0,0 @@
-export const secretSeed="erode cram pudding material viable elephant peasant bean better regular column valid warfare village primary window fire emerge purity uncle movie ticket dust load";
